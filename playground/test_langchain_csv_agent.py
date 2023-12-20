6ddbc667492a0c28560ebded8261f39cdec5cb57
Hazel Guo <hazel.gzh@gmail.com>
2023-12-20 11:16:44 -0800

    test_langchain_csv_agent.py

---
+++ playground/test_langchain_csv_agent.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from langchain.agents.agent_types import AgentType
+from langchain.llms import OpenAI
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+
+
+FILE_PATH1 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet1.csv"
+FILE_PATH2 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet2.csv"
+
+csv_agent = create_csv_agent(
+    OpenAI(temperature=0),
+   [FILE_PATH1, FILE_PATH2],
+   verbose=True,
+   agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+
+print(csv_agent.run('Join the two tables by provider. Return the price list link for providers at location A'))
