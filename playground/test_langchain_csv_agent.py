70efda91df510281837fba6403a12b8d56646814
Hazel Guo <hazel.gzh@gmail.com>
2023-12-20 13:54:37 -0800

    add cost call back

--- playground/test_langchain_csv_agent.py
+++ playground/test_langchain_csv_agent.py
@@ -3,15 +3,20 @@
 from langchain.agents.agent_types import AgentType
 from langchain.llms import OpenAI
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain.callbacks import get_openai_callback
 
 
 FILE_PATH1 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet1.csv"
 FILE_PATH2 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet2.csv"
 
-csv_agent = create_csv_agent(
-   OpenAI(temperature=0),
-   [FILE_PATH1, FILE_PATH2],
-   verbose=True,
-   agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
-print(csv_agent.run('Join the two tables by provider. Return the price list link for providers at location A'))
+with get_openai_callback() as cb:
+    csv_agent = create_csv_agent(
+        OpenAI(temperature=0),
+        [FILE_PATH1, FILE_PATH2],
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+
+    print(csv_agent.run('Join the two tables by provider. Return the price list link for providers at location A'))
+
+    print(cb)
--- playground/test_langchain_df_agent.py
+++ playground/test_langchain_df_agent.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from langchain.llms import OpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.callbacks import get_openai_callback
 
 FILE_PATH1 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet1.csv"
 FILE_PATH2 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet2.csv"
@@ -11,7 +12,9 @@ df1 = pd.read_csv(FILE_PATH1)
 df2 = pd.read_csv(FILE_PATH2)
 df3 = pd.merge(df1, df2, on="Provider")
 
-agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df3, verbose=True)
+with get_openai_callback() as cb:
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df3, verbose=True)
 
-print(agent.run("Return the price list link for providers at location A"))
+    print(agent.run("Return the price list link for providers at location A"))
 
+    print(cb)
