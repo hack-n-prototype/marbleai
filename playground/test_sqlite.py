c914e29a3c7b33881ba2d167f8e6b0b704023b67
Hazel Guo <hazel.gzh@gmail.com>
2023-12-27 13:40:41 -0800

    update test_sqlite to cleanup data first

--- playground/test_sqlite.py
+++ playground/test_sqlite.py
@@ -4,40 +4,71 @@ import sqlite3
 import pandas
 import openai
 from langchain.callbacks import get_openai_callback
+import re
 
-FILE_PATH1 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet1.csv"
-FILE_PATH2 = "/Users/hazelguo/Downloads/Healthcare Price Lists - Sheet2.csv"
-SYSTEM_PROMPT = "You help users write SQL queries."
+MODEL = "gpt-4"
 
-def get_col_names(cur, table_name):
-    cur.execute(f"SELECT * FROM {table_name} LIMIT 0")
-    column_names = [description[0] for description in cur.description]
-    return column_names
+FILE_PATH = "/Users/hazelguo/Downloads/tmp.csv"
+
+PY_SYSTEM_PROMPT = "You help users convert CSV into DB"
+SQL_SYSTEM_PROMPT = "You help users write SQL queries."
+
+def extract_code_from_string(text):
+    # Regular expression to find code blocks wrapped in triple backticks
+    pattern = r"```(?:\w*\n)?(.*?)```"
+    # Using re.DOTALL to make '.' match any character including newline
+    matches = re.findall(pattern, text, re.DOTALL)
+    # Stripping leading and trailing whitespace from each match
+    # stripped_matches = [match.strip() for match in matches]
+    return matches[0]
+
+
+def cleanup_csv(path):
+    df = pandas.read_csv(path)
+
+    prompt = f"""
+I have one CSV file at path "{path}". Here are the first 3 rows: 
+{df.head(3)}
+
+I want to cleanup and format the csv so that I can save it to a database. 
+
+Return python script to read from path, format the CSV, and write back to original path. 
+    """
+
+    with get_openai_callback() as cb:
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=[{"role": "system", "content": PY_SYSTEM_PROMPT},
+                      {"role": "user", "content": prompt}])
+    print(cb)
+
+    res = response["choices"][0]["message"]["content"]
+    print(res)
+
+    code = extract_code_from_string(res)
+    try:
+        exec(code)
+    except:
+        print("having trouble cleaning up data.")
+
+
+cleanup_csv(FILE_PATH)
 
 
 # Create your connection.
 cnx = sqlite3.connect(':memory:')
 
-df1 = pandas.read_csv(FILE_PATH1)
+df1 = pandas.read_csv(FILE_PATH)
 df1.to_sql(name="table1", con=cnx, index=False)
 
-df2 = pandas.read_csv(FILE_PATH2)
-df2.to_sql(name="table2", con=cnx, index=False)
+query = "total of local_ntv"
 
-# execute SQL
 cur = cnx.cursor()
 
-# Execute a query
-
-col_name1 = get_col_names(cur, "table1")
-col_name2 = get_col_names(cur, "table2")
-query = "Return the price list link for providers at location A"
-
-
 prompt = f"""
-I have two tables. 
-'table1' has columns -- {','.join(col_name1)}. 
-'table2' has columns -- {','.join(col_name2)}. 
+I have one table. Here are sample rows:
+table1
+{df1.head(3)}
 
 Now user asks: {query}.
 
@@ -48,19 +79,17 @@ print(prompt)
 
 with get_openai_callback() as cb:
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "system", "content": SYSTEM_PROMPT},
+        model=MODEL,
+        messages=[{"role": "system", "content": SQL_SYSTEM_PROMPT},
                   {"role": "user", "content": prompt}])
+
+print(cb)
 
 sql_query = response["choices"][0]["message"]["content"]
 print(sql_query)
 
 res = cur.execute(sql_query)
 
-
 result = res.fetchall()
 print(type(result))
 print(str(result))
-print(result)
-
-print(cb)
